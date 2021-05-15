250b3db1b67977fb3d7d094e96378c56a1c5784c
Thanwarin Pisanprechatam <thanwarin.pisanprechatam@scb.co.th>
2021-05-15 20:47:42 +0700

    add: prettier, eslint, parcel and gitignore files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.region),
-  ])
-}
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 // Function to create reusable component, like building a rubber stamp to stamp some where else
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", { id: "adopt-me-logo" }, "Adopt Me!"),
-      // ...["Three", "Two", "One"].map((i) => React.createElement("h2", {}, i)),
-      React.createElement(Pet, { name: "Aiya", animal: "Rabbit", region: "Thailand" }),
-      React.createElement(Pet, { name: "Ta-Waan", animal: "Rabbit", region: "Thailand" }),
-      React.createElement(Pet, { name: "Mim", animal: "Cat", region: "Thailand" }),
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "adopt-me-logo" }, "Adopt Me!"),
+    // ...["Three", "Two", "One"].map((i) => React.createElement("h2", {}, i)),
+    React.createElement(Pet, {
+      name: "Aiyaaaaa!",
+      animal: "Rabbit",
+      region: "Thailand",
+    }),
+    React.createElement(Pet, {
+      name: "Ta-Waan",
+      animal: "Rabbit",
+      region: "Thailand",
+    }),
+    React.createElement(Pet, {
+      name: "Miiiiim",
+      animal: "Cat",
+      region: "Thailand",
+    }),
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
